7d0a18eb4e3541df6b86ca5dc0ae0b84aaf4edd8
GaroxXiz <maulanarizwan84@gmail.com>
2026-06-29 18:32:03 +0700

    feat: implement password verification prior to login OTP sending

--- Dtos/VerifyOtpRequest.cs
+++ Dtos/VerifyOtpRequest.cs
@@ -5,4 +5,5 @@ public class VerifyOtpRequest
     public string Email { get; set; } = string.Empty;
     public string Otp { get; set; } = string.Empty;
     public string? Name { get; set; }
+    public string? Password { get; set; }
 }
